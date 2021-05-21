6213d0486b74324fb39df0169e7f70053998c155
Satwika <penugonda.satwika@gmail.com>
2021-05-21 17:41:33 +0530

    Exercise on React Route

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Home(props){
+    return(
+        <div className="container">
+            <p>Home</p>
+        </div>
+    )
+}
+export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
-import Menu from './MenuComponent';
-import {DISHES} from '../shared/dishes';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import DishDetail from './DishDetailedComponent'
-
-
+import Menu from "./MenuComponent";
+import Home from "./HomeComponent";
+import { DISHES } from "../shared/dishes";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import DishDetail from "./DishDetailedComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 class Main extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      dishes:DISHES,
-      selectedDish:null
+    this.state = {
+      dishes: DISHES,
     };
-  }
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
   }
 
   render() {
-        return (
-            <div>
-              <Header></Header>
-              <Menu dishes={this.state.dishes}
-              onClick={(dishId) => this.onDishSelect(dishId)}/>
-              <DishDetail 
-                dish={this.state.dishes.filter((dish)=>dish.id === this.state.selectedDish)[0]}/>
-              <Footer/>
-            </div>
-        );
+    const HomePage=()=>{
+      return(
+        <Home/>
+      )
     }
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home"/>
+          <Route/>
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 export default Main;
